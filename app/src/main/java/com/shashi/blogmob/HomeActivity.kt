4ee1a6f32f3df79ec5fe423c7dc93f7f78d13754
shashi-kant10 <shashiofficial10@gmail.com>
2023-07-25 12:29:35 +0530

    Added logout menu

--- app/src/main/java/com/shashi/blogmob/HomeActivity.kt
+++ app/src/main/java/com/shashi/blogmob/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.shashi.blogmob
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,21 +23,11 @@ class HomeActivity : AppCompatActivity(), IPostAdapter {
     private lateinit var postDao: PostDao
     private lateinit var adapter: PostAdapter
     private lateinit var recyclerView: RecyclerView
+    private lateinit var createPost: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        var createPost = findViewById<FloatingActionButton>(R.id.btn_create_post)
-        createPost.setOnClickListener {
-            val intent = Intent(this, PostActivity::class.java)
-            startActivity(intent)
-        }
-
-        var logout = findViewById<Button>(R.id.btn_logout)
-        logout.setOnClickListener {
-            logout()
-        }
 
         initializeUI()
 
@@ -55,36 +47,78 @@ class HomeActivity : AppCompatActivity(), IPostAdapter {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+        // create post
+        createPost = findViewById(R.id.btn_create_post)
+        createPost.setOnClickListener {
+            val intent = Intent(this, PostActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        adapter.notifyDataSetChanged()
+    }
+
+    // Handle post like
+    override fun onLikeClicked(postId: String) {
+        postDao.updateLikes(postId)
+    }
+
+    // Handle logout
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.home_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val menuId = item.itemId
+        if (menuId == R.id.item_logout)
+            createDialog()
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun createDialog() {
+
+        var builder = AlertDialog.Builder(this)
+        builder.setTitle("Logout confirmation")
+        builder.setTitle("Are you sure you want to logout")
+        builder.setCancelable(false)
+
+        builder.setPositiveButton("Yes") { dialog, which ->
+            logout()
+        }
+
+        builder.setNegativeButton("No") { dialog, which ->
+            builder.create().dismiss()
+        }
+
+        builder.show()
+
     }
 
     private fun logout() {
         FirebaseAuth.getInstance().signOut();
         startActivity(Intent(this, SignInActivity::class.java))
         finish()
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        var auth = Firebase.auth
-        val currentUser = auth.currentUser
-        if (currentUser == null) {
-            startActivity(Intent(this, SignInActivity::class.java))
-        }
-
-        adapter.startListening()
-    }
-
-    override fun onStop() {
-        super.onStop()
-
-        adapter.stopListening()
-    }
-
-    override fun onLikeClicked(postId: String) {
-
-        postDao.updateLikes(postId)
-
     }
 
 }
